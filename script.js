16d5421785a8db44f525907030f2ad438f35227b
Brk <brkgamerr@gmail.com>
2025-04-07 16:26:13 +0200

    Added FAQ section functionality.

--- script.js
+++ script.js
@@ -12,9 +12,43 @@ const sidebarMenuContactButton = document.getElementById("sidebarMenuContactButt
 const sidebarMenuFAQButton = document.getElementById("sidebarMenuFAQButton");
 const sidebarMenuClosingButton = document.getElementById("sidebarMenuClosingButton");
 
-// Sidebar Menu Function
+// FAQ Variables
+const faqQuestion1 = document.getElementById("faqQuestion1");
+const faqQuestion2 = document.getElementById("faqQuestion2");
+const faqQuestion3 = document.getElementById("faqQuestion3");
+const faqQuestion4 = document.getElementById("faqQuestion4");
+const faqQuestion5 = document.getElementById("faqQuestion5");
+const faqQuestion6 = document.getElementById("faqQuestion6");
+
+const faqAnswer1 = document.getElementById("faqAnswer1");
+const faqAnswer2 = document.getElementById("faqAnswer2");
+const faqAnswer3 = document.getElementById("faqAnswer3");
+const faqAnswer4 = document.getElementById("faqAnswer4");
+const faqAnswer5 = document.getElementById("faqAnswer5");
+const faqAnswer6 = document.getElementById("faqAnswer6");
+
+// Sidebar Menu Functions
+function openSidebarMenu() {
+	sidebarMenu.classList.remove("hide");
+}
 function closeSidebarMenu() {
-	sidebarMenu.style.display = "none";
+	sidebarMenu.classList.add("hide");
+}
+
+// FAQ Functions
+function faqQuestionClick(faqAnswer) {
+	if (faqAnswer.classList.contains("hide")) {
+		faqAnswer.classList.remove("hide");
+		const openAnswer = document.querySelectorAll("p.open");
+		openAnswer.forEach(answer => {
+			answer.classList.remove("open");
+			answer.classList.add("hide");
+		})
+		faqAnswer.classList.add("open");
+	} else if (faqAnswer.classList.contains("open")) {
+		faqAnswer.classList.remove("open");
+		faqAnswer.classList.add("hide");
+	}
 }
 
 // Body Event Listener
@@ -30,10 +64,7 @@ window.addEventListener("scroll", () => {
 })
 
 // Sidebar Menu Event Listeners
-sidebarMenuIcon.addEventListener("click", () => {
-	const sidebarMenu = document.querySelector(".sidebar-menu");
-	sidebarMenu.style.display = "flex";
-})
+sidebarMenuIcon.addEventListener("click", openSidebarMenu);
 sidebarMenuFeaturesButton.addEventListener("click", () => {
 	document.getElementById("featuresSection").scrollIntoView({behavior: "smooth"});
 	closeSidebarMenu();
@@ -55,3 +86,23 @@ sidebarMenuFAQButton.addEventListener("click", () => {
 	closeSidebarMenu();
 })
 sidebarMenuClosingButton.addEventListener("click", closeSidebarMenu)
+
+// FAQ Question Event Listeners
+faqQuestion1.addEventListener("click", () => {
+	faqQuestionClick(faqAnswer1);
+})
+faqQuestion2.addEventListener("click", () => {
+	faqQuestionClick(faqAnswer2);
+})
+faqQuestion3.addEventListener("click", () => {
+	faqQuestionClick(faqAnswer3);
+})
+faqQuestion4.addEventListener("click", () => {
+	faqQuestionClick(faqAnswer4);
+})
+faqQuestion5.addEventListener("click", () => {
+	faqQuestionClick(faqAnswer5);
+})
+faqQuestion6.addEventListener("click", () => {
+	faqQuestionClick(faqAnswer6);
+})
